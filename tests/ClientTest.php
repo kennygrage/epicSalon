ad048986fb236829968662db012108409cbcccd6
Kenny Grage <student@epicodus.com>
2015-08-21 10:55:40 -0700

    pass 4 client tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -73,12 +73,35 @@
 
             //Act
             $result = $test_client->getClientName();
-            
+
             //Assert
             $this->assertEquals($client_name, $result);
         }
 
+        //4. Enter a stylist and two clients and test if we saved the 2 clients.
+        function test_save() {
+            //Arrange
+            $stylist_name = "Allison";
+            $id = null;
+            $test_stylist = new Stylist($stylist_name, $id);
+            $test_stylist->save();
 
+            $client_name = "Pablo";
+            $stylist_id = $test_stylist->getId();
+            $test_client = new Client($client_name, $id, $stylist_id);
+            $test_client->save();
+
+            $client_name2 = "Paco";
+            $stylist_id2 = $test_stylist->getId();
+            $test_client2 = new Client($client_name2, $id, $stylist_id2);
+            $test_client2->save();
+
+            //Act
+            $result = Client::getAll();
+
+            //Assert
+            $this->assertEquals([$test_client, $test_client2], $result);
+        }
 
     }
 ?>
