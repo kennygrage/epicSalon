6238ba2a88f7b68cbff1779a6c98ea88fd8f09e0
Kenny Grage <student@epicodus.com>
2015-08-21 11:07:38 -0700

    pass 8 client tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -180,6 +180,31 @@
             $this->assertEquals($test_client, $result);
         }
 
+        //8. Enter a stylist and two clients and sort the clients.
+        function test_sort() {
+            //Arrange
+            $stylist_name = "Allison";
+            $id = null;
+            $test_stylist = new Stylist($stylist_name, $id);
+            $test_stylist->save();
+
+            $client_name = "Paco";
+            $stylist_id = $test_stylist->getId();
+            $test_client = new Client($client_name, $id, $stylist_id);
+            $test_client->save();
+
+            $client_name2 = "Pablo";
+            $stylist_id2 = $test_stylist->getId();
+            $test_client2 = new Client($client_name2, $id, $stylist_id2);
+            $test_client2->save();
+
+            //Act
+            $id = $test_client->getId();
+            $result = Client::getAll();
+
+            //Assert
+            $this->assertEquals([$test_client2, $test_client], $result);
+        }
 
 
     }
